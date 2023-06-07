9be4ef9550d8ad3941160951f2e4fb0bac365ce5
Shivam Rawat <ajsivamaj@gmail.com>
2023-06-07 10:36:10 +0530

    Project complete

--- codepen-clone/src/components/Result.jsx
+++ codepen-clone/src/components/Result.jsx
@@ -1,8 +1,18 @@
 
 import { Box, styled } from '@mui/material'
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { DataContext } from '../context/DataProvider'
+
+const Container=styled(Box)`
+
+    height:41vh
+
+`
+
+
 const Result = () => {
+
+  const [src, setSrc] = useState('');
 
   const { html, css, js } = useContext(DataContext);
 
@@ -15,18 +25,29 @@ const Result = () => {
     </html>
 
   `
-   return(
-    <Box>
+
+useEffect(()=>{
+  const timeout=setTimeout(()=>{
+
+    setSrc(srcCode);
+
+  },1000)
+
+  return()=>clearTimeout(timeout);
+},[html,css,js])
+
+  return (
+    <Container>
       <iframe
-      srcDoc={srcCode}
-      title='Output'
-      sandbox='allow-scripts'
-      frameBorder={0}
-      width='100%'
-      height='100%'
+        srcDoc={src}
+        title='Output'
+        sandbox='allow-scripts'
+        frameBorder={0}
+        width='100%'
+        height='100%'
       />
-    </Box>
-   )
+    </Container>
+  )
 }
 
 export default Result;
